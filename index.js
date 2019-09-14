79308476b1361f64a2dcb3370ddc4733b2e8da35
bhavayanand <bhavay.anand@intelliticks.com>
2019-09-14 17:46:52 +0530

    r

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ var fs = require('fs')
 var morgan = require('morgan')
 var path = require('path')
 
-// create a write stream (in append mode)
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
- 
-// setup the logger
+const child_process = require('child_process');
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' }) 
 app.use(morgan('combined', { stream: accessLogStream }))
 
-// viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
@@ -19,9 +17,16 @@ app.get('/', function(req, res) {
 app.post('/push-event', function(req, res){
     try{
         if(req.query.password === "8190"){
-                // res.sendFile(path.join(__dirname + '/access.log'));
-                exec('cd ~/bhavayAnand9.github.io && git pull', function(error, stdout, stderr) {
-                });
+
+            console.log('Spawning sync.sh');
+            const process = child_process.spawn('./sync.sh');
+            process.on('exit', () => {
+                console.log('process exit');
+            });
+            process.stdout.on('data', (data) => {
+                console.log('Output: ' + data.toString('utf8'));
+            });
+
         } else {
             res.send();
         }
---
+++ sync.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "inside sync"
+cd ~/bhavayAnand9.github.io && git pull
