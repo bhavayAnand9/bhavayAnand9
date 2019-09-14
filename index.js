c23242a983045d69d422b8008abb516551a710cb
Ubuntu <ubuntu@ip-172-31-6-34.ap-south-1.compute.internal>
2019-09-14 11:00:39 +0000

    g

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs')
+var morgan = require('morgan')
+var path = require('path')
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+ 
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(80);
