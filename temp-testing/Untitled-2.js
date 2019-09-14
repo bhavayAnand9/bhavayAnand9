1838715e30f3665269612306ffac3cd80722cc4c
bhavayanand <bhavay.anand@intelliticks.com>
2019-09-15 00:10:43 +0530

    tt

---
+++ temp-testing/Untitled-2.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var fs = require('fs')
+var morgan = require('morgan')
+var path = require('path')
+
+const http = require('http');
+const https = require('https');
+
+const options = {
+    cert: fs.readFileSync('./sslcert/fullchain.pem'),
+    key: fs.readFileSync('./sslcert/privkey.pem')
+};
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' }) 
+app.use(morgan('combined', { stream: accessLogStream }))
+
+// app.use(express.static(__dirname, { dotfiles: 'allow' } ));
+app.use(express.static(__dirname + '/static', { dotfiles: 'allow' } ))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/logs', function(req, res) {
+    try{
+        if(req.query.password === "8190"){
+            res.sendFile(path.join(__dirname + '/access.log'));
+        } else {
+            res.send();
+        }
+    } catch(e){
+        res.send(e);
+    }
+});
+
+app.listen(80);
+
+https.createServer(options, app).listen(443);
