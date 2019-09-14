90f0197f134f1b1b893a00e8675c79f87b31bf58
bhavayanand <bhavay.anand@intelliticks.com>
2019-09-14 17:28:01 +0530

    kk

--- index.js
+++ index.js
@@ -16,9 +16,23 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.post('/push-event', function(req, res){
+    try{
+        if(req.query.password === "8190"){
+                // res.sendFile(path.join(__dirname + '/access.log'));
+                exec('cd ~/bhavayAnand9.github.io && git pull', function(error, stdout, stderr) {
+                });
+        } else {
+            res.send();
+        }
+    } catch(e){
+        res.send(e);
+    }
+});
+
 app.get('/logs', function(req, res) {
     try{
-        if(req.query.password === "bhavay"){
+        if(req.query.password === "8190"){
             res.sendFile(path.join(__dirname + '/access.log'));
         } else {
             res.send();
